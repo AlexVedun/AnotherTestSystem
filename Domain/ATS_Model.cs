d947a99c95ca47246388540469d033af259ea2a7
Efremenko Alexey <alexeyefr@gmail.com>
2019-01-29 22:39:58 +0200

    Domain: настройки

---
+++ Domain/ATS_Model.cs
@@ -0,0 +1,31 @@
+﻿namespace Domain
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class ATS_Model : DbContext
+    {
+        // Контекст настроен для использования строки подключения "ATS_Model" из файла конфигурации  
+        // приложения (App.config или Web.config). По умолчанию эта строка подключения указывает на базу данных 
+        // "Domain.ATS_Model" в экземпляре LocalDb. 
+        // 
+        // Если требуется выбрать другую базу данных или поставщик базы данных, измените строку подключения "ATS_Model" 
+        // в файле конфигурации приложения.
+        public ATS_Model()
+            : base("name=ATS_Model")
+        {
+        }
+
+        // Добавьте DbSet для каждого типа сущности, который требуется включить в модель. Дополнительные сведения 
+        // о настройке и использовании модели Code First см. в статье http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
